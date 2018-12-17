0718e938c692cd4b8690c9b662c8cf167ec05ce7
kurt <kurt@hust.edu.cn>
2018-12-17 14:55:00 +0800

    delete isSplitable function

--- src/main/java/CWInputFormat.java
+++ src/main/java/CWInputFormat.java
@@ -1,7 +1,5 @@
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -10,11 +8,6 @@ import java.io.IOException;
 
 // 计算条件概率时,将<classname, word>作为Mapper的输入
 public class CWInputFormat extends FileInputFormat<PairWritable, IntWritable> {
-
-    @Override
-    protected boolean isSplitable(JobContext context, Path filename) {
-        return false;
-    }
 
     @Override
     public RecordReader<PairWritable, IntWritable> createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
